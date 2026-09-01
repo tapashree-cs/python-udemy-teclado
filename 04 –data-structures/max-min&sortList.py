d290f3a5b079af8f566f92be0d4c094f0f491efd
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-01 23:46:51 +0530

    Find the maximum and minimum values from a list & Sort a list of numbers in ascending and descending order.

---
+++ 04 –data-structures/max-min&sortList.py
@@ -0,0 +1,10 @@
+numbers = [50, 10, 40, 20, 30]
+
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
+
+numbers.sort()
+print("Ascending:", numbers)
+
+numbers.sort(reverse=True)
+print("Descending:", numbers)
