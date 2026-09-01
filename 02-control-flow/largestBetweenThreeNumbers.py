0eaa6a921460b100a7d0f76890456c67a031d8ee
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-01 20:01:30 +0530

    Largest Between Three Numbers

---
+++ 02-control-flow/largestBetweenThreeNumbers.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest =", a)
+elif b >= a and b >= c:
+    print("Largest =", b)
+else:
+    print("Largest =", c)
