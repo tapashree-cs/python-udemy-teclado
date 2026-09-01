8e2b45b0aa336f93265af85a6852c9dae98f318b
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-01 21:29:04 +0530

    Slicing Operations

---
+++ 03-strings/stringSlicing.py
@@ -0,0 +1,9 @@
+text = "Python Programming"
+
+print(text[0:5])
+print("First five characters:", text[:5])
+print("Last five characters:", text[-5:])
+
+reverse = text[::-1]
+print("Reversed string:", reverse)
+
