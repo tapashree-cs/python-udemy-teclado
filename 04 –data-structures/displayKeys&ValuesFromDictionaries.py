bfdb502a8b416aba6294e757cc9b46e0e48ff7a8
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-02 23:55:24 +0530

    displayKeys&ValuesFromDictionaries

---
+++ 04 –data-structures/displayKeys&ValuesFromDictionaries.py
@@ -0,0 +1,15 @@
+student = {
+    "name": "Tapashree",
+    "age": 21,
+    "course": "Computer Science"
+}
+
+print("Keys:")
+
+for key in student.keys():
+    print(key)
+
+print("Values:")
+
+for value in student.values():
+    print(value)
