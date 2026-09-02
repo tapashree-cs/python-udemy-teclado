58305a92dbcde521742d9c3c7e86d8ddec253c93
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-02 23:25:33 +0530

    Print set elements  & remove Duplicate

---
+++ 04 –data-structures/print&removeDuplicateFromSets.py
@@ -0,0 +1,7 @@
+nums = {10, 20, 30, 40, 50}
+print(nums)
+
+numbers = [10, 20, 10, 30, 20, 40, 30]
+unique_numbers = set(numbers)
+print("Original:", numbers)
+print("Without duplicates:", unique_numbers)
