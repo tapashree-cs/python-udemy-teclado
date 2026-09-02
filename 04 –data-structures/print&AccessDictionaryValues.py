f6e21f274f267086320b730c866a8db964cb99fc
Tapashree Mukherjee <tapashreemukherjee80@outlook.com>
2026-09-02 23:41:21 +0530

    Print & Access Dictionary Values

---
+++ 04 –data-structures/print&AccessDictionaryValues.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Tapashree",
+    "age": 21,
+    "course": "Computer Science"
+}
+
+print(student)
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
